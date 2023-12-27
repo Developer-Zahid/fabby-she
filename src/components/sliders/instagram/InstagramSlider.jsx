62d85ae2463a30390de04940d9c7cc22628c2d49
Developer-Zahid <coderexpertzahid@gmail.com>
2023-12-27 17:15:46 +0600

    Added instagram section

--- src/components/sliders/instagram/InstagramSlider.jsx
+++ src/components/sliders/instagram/InstagramSlider.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Image from '../../image/Image'
 import InstagramIcon from '../../../assets/icons/InstagramIcon'
-import './_instagramSlider.scss'
+import './instagramSlider.scss'
 
 export default function InstagramSlider({slides}) {
   return (
@@ -10,7 +10,21 @@ export default function InstagramSlider({slides}) {
         <div className="instagram__slider__row">
             {
                 slides.map(item=>(
-                    <Link key={item.id} to={item.link} className="instagram__slide">
+                    <Link key={item.id} to={item.link} target="_blank" className="instagram__slide">
+                        <figure className="instagram__figure">
+                            <Image className="instagram__figure__bg" src={item.imageSrc} alt={item.imageAlt} />
+                        </figure>
+                        <span className="instagram__slide__icon">
+                            <InstagramIcon />
+                        </span>
+                    </Link>
+                ))
+            }
+        </div>
+        <div className="instagram__slider__row" aria-hidden="true">
+            {
+                slides.map(item=>(
+                    <Link key={item.id} to={item.link} target="_blank" className="instagram__slide" tabIndex="-1">
                         <figure className="instagram__figure">
                             <Image className="instagram__figure__bg" src={item.imageSrc} alt={item.imageAlt} />
                         </figure>
