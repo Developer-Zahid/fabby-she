d7e30d53ca36c040cd923d566932318fb70ad168
Developer-Zahid <coderexpertzahid@gmail.com>
2023-12-29 19:33:21 +0600

    Fix path issue

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 import RootLayout from "./layouts/main/RootLayout";
 import CategoryPage from "./pages/category/CategoryPage";
-import ClientError from "./pages/errors/clientError";
+import ClientError from "./pages/errors/ClientError";
 import HomePage from "./pages/home/HomePage";
 import ShopePage from "./pages/shop/ShopPage";
   
